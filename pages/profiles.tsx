28228b22f8cf3713890a9f670476148fb6f46f2a
Subhatav Dhali <subhatav@gmail.com>
2023-07-07 22:55:25 +0530

    Create the Profiles page in the `profiles` TSX file

---
+++ pages/profiles.tsx
@@ -0,0 +1,51 @@
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+import UserCard from "@/components/UserCard";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+const Profiles = () => {
+  const router = useRouter();
+
+  const { data: currentUser } = useCurrentUser();
+
+  const selectProfile = useCallback(() => {
+    router.push("/");
+  }, [router]);
+
+  return (
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who&#39;s watching?
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => selectProfile()}>
+            <UserCard name={currentUser?.name} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profiles;
