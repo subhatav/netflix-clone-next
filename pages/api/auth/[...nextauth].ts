821b5520bcb54fc8228154cdf448b4c956f3f266
Subhatav Dhali <subhatav@gmail.com>
2023-07-07 18:22:24 +0530

    Create the Next-Auth options in the `[...nextauth]` TS file

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,82 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import { compare } from "bcrypt";
+
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import NextAuth from "next-auth/next";
+
+import prismadb from "@/libs/prismadb";
+
+export const options: AuthOptions = {
+  adapter: PrismaAdapter(prismadb),
+
+  pages: {
+    signIn: "/auth",
+  },
+
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password are required!");
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user?.hashedPassword) {
+          throw new Error("Email does not exist!");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Password is incorrect!");
+        }
+
+        return user;
+      },
+    }),
+  ],
+
+  debug: process.env.NODE_ENV === "development",
+
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+};
+
+export default NextAuth(options);
