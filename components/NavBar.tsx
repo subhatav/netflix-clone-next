f1212ae6e04f9a9fd0d2c2bf97bffbd6427b4fe3
Subhatav Dhali <subhatav@gmail.com>
2023-07-08 16:43:48 +0530

    Create the Navigation Bar component in the `NavBar` TSX file

---
+++ components/NavBar.tsx
@@ -0,0 +1,96 @@
+import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
+import { useCallback, useEffect, useState } from "react";
+
+import NavItem from "@/components/NavItem";
+import MobileMenu from "@/components/MobileMenu";
+import AccountMenu from "@/components/AccountMenu";
+
+const TOP_OFFSET = 66;
+
+const NavBar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY);
+
+      setShowBackground(window.scrollY >= TOP_OFFSET);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className="w-full fixed z-40">
+      <div
+        className={`px-4 py-6 md:px-16 flex flex-row items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900 bg-opacity-90" : ""
+        }`}
+      >
+        <img src="/images/logo.png" className="h-4 lg:h-7" alt="Netflix Logo" />
+
+        <div className="flex-row ml-8 gap-7 hidden lg:flex">
+          <NavItem label="Home" />
+          <NavItem label="Series" />
+          <NavItem label="Films" />
+          <NavItem label="New & Popular" />
+          <NavItem label="My List" />
+          <NavItem label="Browse by Languages" />
+        </div>
+
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsBell />
+          </div>
+
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="Profile Logo" />
+            </div>
+            <BsChevronDown
+              className={`w-4 text-white fill-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -74,7 +74,7 @@ const Auth = () => {
     <div className="relative w-full h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" className="h-12" alt="Logo" />
+          <img src="/images/logo.png" className="h-12" alt="Netflix Logo" />
         </nav>
 
         <div className="flex justify-center">
