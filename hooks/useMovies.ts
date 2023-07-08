0c26cb637127af336d2833a81527aed373c41b1e
Subhatav Dhali <subhatav@gmail.com>
2023-07-08 21:11:23 +0530

    Create a Hook to fetch all movies in the `useMovies` TS file

---
+++ hooks/useMovies.ts
@@ -0,0 +1,19 @@
+import useSWR from "swr";
+
+import fetcher from "@/libs/fetcher";
+
+const useMovies = () => {
+  const { data, error, isLoading } = useSWR("/api/movies", fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
+export default useMovies;
