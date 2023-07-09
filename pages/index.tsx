6379c5a81aba9c9c480ce5f9f4a3db1fc915cebb
Subhatav Dhali <subhatav@gmail.com>
2023-07-09 17:29:58 +0530

    Finalize the Page for the landing Home in the `index` TSX file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,50 @@
-export default function Home() {
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+import useInfoModal from "@/hooks/useInfoModal";
+import useMovies from "@/hooks/useMovies";
+import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import NavBar from "@/components/NavBar";
+import BillBoard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+const Home = () => {
+  const { data: movies = [] } = useMovies();
+  const { data: favorites = [] } = useFavorites();
+
+  const { isOpen, closeModal } = useInfoModal();
+
   return (
     <>
-      <h1>Netflix Clone</h1>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+
+      <NavBar />
+      <BillBoard />
+
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   );
-}
+};
+
+export default Home;
